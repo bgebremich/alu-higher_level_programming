e2f7b911a33c3db39c71f46f38469a708198ea81
Bemnet Gebremichael <b.gebremich@alustudent.com>
2026-06-26 11:24:31 -0700

    Add python-hello_world task 0: run Python file

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str3 = "object-oriented programming with Python"
-print(str3[:19] + str3[24:])
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+print(str[44:71] + str[77:86])
---
+++ python-hello_world/README.md
@@ -0,0 +1,3 @@
+# Python - Hello, World
+
+This project covers the basics of Python programming including printing, strings, and the Python interpreter.

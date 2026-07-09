5150a5648f8d389a85f81ff6bb12e01bd8324dda
Bemnet Gebremichael <b.gebremich@alustudent.com>
2026-07-09 00:02:09 -0700

    fix

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-<<<<<<< HEAD
+def print_list_integer(my_list=[])
     for i in my_list:
-        print("{:d}".format(i)) 
-=======
-    for item in my_list:
-        print("{:d}".format(item))
-
->>>>>>> 82a00dba7c2bfc2bdad29c8a58ab3c1ddaa42084
+        print("{:d}".format(i))

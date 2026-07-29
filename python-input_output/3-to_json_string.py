61970670af495c3f8633e70d5f27f449b490dcaf
Bemnet Tenketem <b.gebremich@alustudent.com>
2026-07-29 18:24:12 +0200

    Complete tasks 0-4

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for converting a Python object to a JSON string."""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object."""
+    return json.dumps(my_obj)
